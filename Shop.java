abf236f13224a6a32eccdbe50cbe9f85b9fdac90
Parth Patel <patelhparth@gmail.com>
2023-10-03 18:22:12 -0600

    r1: Extract Method sellItems() in the Shop class

--- Shop.java
+++ Shop.java
@@ -1,37 +1,95 @@
 public class Shop {
     public double profit;
 
-
-    public void sellItems(Item item, int quantityToSell) throws Exception {
-        if (item.quantity ==  0){
-            throw new Exception("Item is sold out");
+    /**
+     * Check if item is available and valid (not expired),
+     * @param item: item to sell
+     * @param quantityToSell: quantity of items to sell
+     * @return true if item is available and valid (not expired)
+     * @throws Exception: if item is not available to sell or if a food item is expired
+     */
+    public boolean itemAvailableAndValid(Item item, int quantityToSell) throws Exception{
+        if(item.quantity == 0){
+            throw new Exception("Item is currently sold out");
         }
+
         else if(item.quantity < quantityToSell){
             throw new Exception(String.format("Not enough items available to sell, only %s in stock", item.quantity));
         }
+
         else if(item.daysLastBought >= 10 && item.name.startsWith("Food")){
-            throw new Exception("Item cannot be sold as it has expired");
+            throw new Exception("Food item cannot be sold as it has expired");
         }
-        else{
-            if (item.daysLastBought >= 30){
-                if(item.discountRate <= 40){
-                    if(item.name.startsWith("Clothes")){
-                        item.discountRate += (item.daysLastBought/ 40) * 5;
-                    }
-                    else if(item.name.startsWith("Electronics")){
-                        item.discountRate += (item.daysLastBought / 60) * 3;
-                    }
-                    else {
-                        item.discountRate += (item.daysLastBought / 20) * 2;
-                    }
-                    if(item.discountRate > 40){
-                        item.discountRate = 40;
-                    }
-                }
-                item.sellingPrice *= (item.discountRate / 100);
+
+        else {
+            return true;
+        }
+    }
+
+    /**
+     * Update item discount rate of an item based on the type of item.
+     * Maximum discount rate for any item is 40%
+     * @param item:
+     *            Clothes  - up by 5% every 40 days
+     *            Electronics - up by 3% every 60 days
+     *            Others - up by 2% every 20 days
+     */
+    public void updateItemDiscountRate(Item item) {
+        if (item.name.startsWith("Clothes")) {
+            item.discountRate += (item.daysLastBought / 40) * 5;
+        } else if (item.name.startsWith("Electronics")) {
+            item.discountRate += (item.daysLastBought / 60) * 3;
+        } else {
+            item.discountRate += (item.daysLastBought / 20) * 2;
+        }
+
+        if (item.discountRate >= 40) {
+            item.discountRate = 40;
+        }
+    }
+
+    /**
+     * Update items selling price based on its discountRate
+     * @param item: valid Item
+     */
+    public void applyDiscount(Item item){
+        updateItemDiscountRate(item);
+        item.sellingPrice *= item.discountRate / 100;
+    }
+
+    /**
+     * Update profit after selling an item
+     * Profit could be a negative value to indicate a loss
+     * @param item: item sold
+     * @param quantityToSell: quantity of items sold
+     */
+    public void updateProfit(Item item, int quantityToSell){
+        profit += (quantityToSell * item.sellingPrice) - (item.quantity * item.buyingPrice);
+    }
+
+    /**
+     * Update item inventory after selling the item
+     * @param item: item sold
+     * @param quantityToSell: quantity of items sold
+     */
+    public void updateInventory(Item item, int quantityToSell){
+        item.quantity -= quantityToSell;
+    }
+
+    /**
+     * sell items if they are available and valid
+     * If items were last bought 30 days ago, applyDiscount to item
+     * @param item: item to sell
+     * @param quantityToSell: quantity of items to sell
+     * @throws Exception: If item is not valid or available
+     */
+    public void sellItems(Item item, int quantityToSell) throws Exception {
+        if(itemAvailableAndValid(item, quantityToSell)){
+            if(item.daysLastBought >= 30){
+                applyDiscount(item);
             }
-            profit += (quantityToSell * item.sellingPrice) - (item.quantity * item.buyingPrice);
-            item.quantity -= quantityToSell;
+            updateProfit(item, quantityToSell);
+            updateInventory(item, quantityToSell);
         }
     }
 }
--- ShopTest.java
+++ ShopTest.java
@@ -1,7 +1,7 @@
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ShopTest {
     private static Shop shop;
@@ -11,6 +11,7 @@ public class ShopTest {
     private static Item goodToys2;
     private static Item goodClothes2;
     private static Item goodElectronics;
+    private static Item goodFood;
     private static Item goodDecor;
     private static Item soldOutClothes;
     private static Item lowStockToys;
@@ -20,10 +21,6 @@ public class ShopTest {
     @Before
     public void initShop(){
         shop = new Shop();
-    }
-
-    @BeforeClass
-    public static void init(){
         goodClothes = new Item("Clothes - tshirts", 10.0, 20.0, 5, 40, 30.0);
         goodToys = new Item("Toys - planes", 10.0, 20.0, 5, 40, 30.0);
         goodElectronics = new Item("Electronics - headphones", 100, 300, 5, 120, 30);
@@ -31,10 +28,59 @@ public class ShopTest {
         soldOutClothes = new Item("Clothes - tshirts", 15, 45, 0, 5, 10);
         lowStockToys = new Item("Toy - dolls", 10.0, 20.0, 2, 5, 10.0);
         expiredFood = new Item("Food - potatoes", 10.0, 20.0, 5, 15, 10.0);
+        goodFood = new Item("Food - potatoes", 10.0, 20.0, 5, 5, 10.0);
         maxDiscountItem = new Item("Electronics - headphones", 100, 450, 15, 1000, 40.0);
 
         goodClothes2 = new Item("Clothes - tshirts", 10.0, 20.0, 5, 1, 30.0);
         goodToys2 = new Item("Toys - planes", 10.0, 20.0, 5, 1, 30.0);
+    }
+
+    @Test
+    public void testItemAvailable() throws Exception {
+        assertTrue(shop.itemAvailableAndValid(goodToys, 5));
+        assertTrue(shop.itemAvailableAndValid(goodClothes, 1));
+    }
+
+    @Test(expected = Exception.class)
+    public void testItemNotAvailable() throws Exception{
+        shop.itemAvailableAndValid(lowStockToys, 3);
+    }
+
+    @Test
+    public void testValidFood() throws Exception{
+        assertTrue(shop.itemAvailableAndValid(goodFood, 1));
+    }
+
+    @Test(expected = Exception.class)
+    public void testExpiredFood() throws Exception{
+        shop.itemAvailableAndValid(expiredFood, 1);
+    }
+
+    @Test
+    public void testUpdateItemDiscountRate(){
+        Item[] items = new Item[]{goodClothes, goodToys, goodElectronics, goodDecor};
+
+
+        for(int i=0; i < items.length; i++){
+            shop.updateItemDiscountRate(items[i]);
+        }
+
+        assertEquals(35, goodClothes.discountRate, 0.01);
+        assertEquals(34, goodToys.discountRate, 0.01);
+        assertEquals(36, goodElectronics.discountRate, 0.01);
+        assertEquals(24, goodDecor.discountRate, 0.01);
+    }
+
+    @Test
+    public void testApplyDiscount(){
+        Item[] items = new Item[]{goodClothes, goodToys, goodElectronics, goodDecor};
+        for(int i=0; i < items.length; i++){
+            shop.applyDiscount(items[i]);
+        }
+
+        assertEquals(7, goodClothes.sellingPrice,0.01);
+        assertEquals(6.8, goodToys.sellingPrice, 0.01);
+        assertEquals(108, goodElectronics.sellingPrice, 0.01);
     }
 
     @Test
@@ -90,4 +136,5 @@ public class ShopTest {
 
         assertEquals(1200, shop.profit, 0.01);
     }
+
 }
